fe99cdd62fd61bc87f75c7433a53a53a94be3c30
reGhost00 <32218960+reGhost00@users.noreply.github.com>
2020-07-20 13:30:03 +0800

    initial commit

--- app/router.js
+++ app/router.js
@@ -7,4 +7,9 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function() {
+    this.route("about");
+    this.route("lisu");
+    this.route("auth", {path:""}, function(){
+        this.route("usct");
+    });
 });
